5c96e103130f1149b92aa5699a0ea44205336341
Randy Efan <randyrandyrej8@gmail.com>
2021-10-26 12:14:15 +0700

    Initial Commit

---
+++ Haloto Merchant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Haloto Merchant
+//
+//  Created by Randy Efan Jayaputra on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
